b16fbe25bdf60ffca71f23d5541d880a183b06a5
darren <darren94me@gmail.com>
2022-07-18 09:08:08 +0800

    m

---
+++ learn_stru2/target/debug/deps/learn_stru2-d8f696d48911c0e3.d
@@ -0,0 +1,5 @@
+/Users/darren/Documents/project/rust/Learn_Rust/learn_stru2/target/debug/deps/learn_stru2-d8f696d48911c0e3: src/main.rs
+
+/Users/darren/Documents/project/rust/Learn_Rust/learn_stru2/target/debug/deps/learn_stru2-d8f696d48911c0e3.d: src/main.rs
+
+src/main.rs:
---
+++ learn_stru2/target/debug/learn_stru2.d
@@ -0,0 +1,1 @@
+/Users/darren/Documents/project/rust/Learn_Rust/learn_stru2/target/debug/learn_stru2: /Users/darren/Documents/project/rust/Learn_Rust/learn_stru2/src/main.rs
